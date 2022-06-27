42e0eb6d65dfa322c78476fb10727d325d45075b
Alex Trigolos <alexvlg34@gmail.com>
2022-06-27 11:37:58 +0000

    mb last update filter

--- local/templates/home/header.php
+++ local/templates/home/header.php
@@ -5,7 +5,7 @@ IncludeTemplateLangFile(__FILE__);
 <!DOCTYPE html>
 <html lang="<?=LANGUAGE_ID?>">
 <head>
-    <title>HomeSpace &mdash; Colorlib Website Template</title>
+    <title><?$APPLICATION->ShowTitle();?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <?
@@ -123,49 +123,46 @@ IncludeTemplateLangFile(__FILE__);
 
                         <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
 
-                        <?$APPLICATION->IncludeComponent(
-	"bitrix:menu", 
-	".default", 
-	array(
-		"ALLOW_MULTI_SELECT" => "N",
-		"CHILD_MENU_TYPE" => "left",
-		"DELAY" => "N",
-		"MAX_LEVEL" => "1",
-		"MENU_CACHE_GET_VARS" => array(
-		),
-		"MENU_CACHE_TIME" => "3600",
-		"MENU_CACHE_TYPE" => "A",
-		"MENU_CACHE_USE_GROUPS" => "Y",
-		"ROOT_MENU_TYPE" => "top",
-		"USE_EXT" => "N",
-		"COMPONENT_TEMPLATE" => ".default",
+                        <?$APPLICATION->IncludeComponent("bitrix:menu", "header_multilvl", Array(
+	"ALLOW_MULTI_SELECT" => "N",	// Разрешить несколько активных пунктов одновременно
+		"CHILD_MENU_TYPE" => "left",	// Тип меню для остальных уровней
+		"DELAY" => "N",	// Откладывать выполнение шаблона меню
+		"MAX_LEVEL" => "2",	// Уровень вложенности меню
+		"MENU_CACHE_GET_VARS" => "",	// Значимые переменные запроса
+		"MENU_CACHE_TIME" => "3600",	// Время кеширования (сек.)
+		"MENU_CACHE_TYPE" => "A",	// Тип кеширования
+		"MENU_CACHE_USE_GROUPS" => "Y",	// Учитывать права доступа
+		"ROOT_MENU_TYPE" => "top",	// Тип меню для первого уровня
+		"USE_EXT" => "N",	// Подключать файлы с именами вида .тип_меню.menu_ext.php
+		"COMPONENT_TEMPLATE" => "horizontal_multilevel",
 		"MENU_THEME" => "site"
 	),
 	false
-);?><ul class="site-menu js-clone-nav d-none d-lg-block">
-                            <li class="active">
-                                <a href="<?=SITE_TEMPLATE_PATH?>/index.html">Home</a>
-                            </li>
-                            <li class="has-children">
-                                <a href="<?=SITE_TEMPLATE_PATH?>/properties.html">Properties</a>
-                                <ul class="dropdown">
-                                    <li><a href="#">Buy</a></li>
-                                    <li><a href="#">Rent</a></li>
-                                    <li><a href="#">Lease</a></li>
-                                    <li class="has-children">
-                                        <a href="#">Menu</a>
-                                        <ul class="dropdown">
-                                            <li><a href="#">Menu One</a></li>
-                                            <li><a href="#">Menu Two</a></li>
-                                            <li><a href="#">Menu Three</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li><a href="<?=SITE_TEMPLATE_PATH?>/blog.html">Blog</a></li>
-                            <li><a href="<?=SITE_TEMPLATE_PATH?>/about.html">About</a></li>
-                            <li><a href="<?=SITE_TEMPLATE_PATH?>/contact.html">Contact</a></li>
-                        </ul>
+);?>
+<!--                        <ul class="site-menu js-clone-nav d-none d-lg-block">-->
+<!--                            <li class="active">-->
+<!--                                <a href="--><?//=SITE_TEMPLATE_PATH?><!--/index.html">Home</a>-->
+<!--                            </li>-->
+<!--                            <li class="has-children">-->
+<!--                                <a href="--><?//=SITE_TEMPLATE_PATH?><!--/properties.html">Properties</a>-->
+<!--                                <ul class="dropdown">-->
+<!--                                    <li><a href="#">Buy</a></li>-->
+<!--                                    <li><a href="#">Rent</a></li>-->
+<!--                                    <li><a href="#">Lease</a></li>-->
+<!--                                    <li class="has-children">-->
+<!--                                        <a href="#">Menu</a>-->
+<!--                                        <ul class="dropdown">-->
+<!--                                            <li><a href="#">Menu One</a></li>-->
+<!--                                            <li><a href="#">Menu Two</a></li>-->
+<!--                                            <li><a href="#">Menu Three</a></li>-->
+<!--                                        </ul>-->
+<!--                                    </li>-->
+<!--                                </ul>-->
+<!--                            </li>-->
+<!--                            <li><a href="--><?//=SITE_TEMPLATE_PATH?><!--/blog.html">Blog</a></li>-->
+<!--                            <li><a href="--><?//=SITE_TEMPLATE_PATH?><!--/about.html">About</a></li>-->
+<!--                            <li><a href="--><?//=SITE_TEMPLATE_PATH?><!--/contact.html">Contact</a></li>-->
+<!--                        </ul>-->
                     </nav>
                 </div>
 
@@ -205,3 +202,70 @@ IncludeTemplateLangFile(__FILE__);
     </div>
 
 </div>
+<?$arrFilter = ['PROPERTY_Priority_deal' => 'Priority_deal'];
+$APPLICATION->IncludeComponent(
+	"bitrix:news.list", 
+	".default", 
+	array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"CACHE_FILTER" => "N",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
+		"FILTER_NAME" => "arrFilter",
+		"HIDE_LINK_WHEN_NO_DETAIL" => "N",
+		"IBLOCK_ID" => "5",
+		"IBLOCK_TYPE" => "ads",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"INCLUDE_SUBSECTIONS" => "Y",
+		"MESSAGE_404" => "",
+		"NEWS_COUNT" => "20",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_DESC_NUMBERING" => "N",
+		"PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_SHOW_ALWAYS" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Объявления",
+		"PARENT_SECTION" => "",
+		"PARENT_SECTION_CODE" => "",
+		"PREVIEW_TRUNCATE_LEN" => "",
+		"PROPERTY_CODE" => array(
+			0 => "",
+			1 => "",
+			2 => "",
+		),
+		"SET_BROWSER_TITLE" => "Y",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "Y",
+		"SET_META_KEYWORDS" => "Y",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "Y",
+		"SHOW_404" => "N",
+		"SORT_BY1" => "ACTIVE_FROM",
+		"SORT_BY2" => "SORT",
+		"SORT_ORDER1" => "DESC",
+		"SORT_ORDER2" => "ASC",
+		"STRICT_SECTION_CHECK" => "N",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
+);?>
